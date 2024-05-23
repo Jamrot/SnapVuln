d7ca26fd7a7a63d3b6f9ef6868e3a8f2e2e8c598
root <jm.jamrot@gmail.com>
2024-05-23 03:03:58 +0000

    update

--- my-everthing/src/patch_process/patch_analyzer.py
+++ my-everthing/src/patch_process/patch_analyzer.py
@@ -8,6 +8,7 @@ class PatchAnalyzer:
         self.url = url
         self.hash = hash
         self.repo = Git(self.url)
+        self.commit = self.repo.get_commit(self.hash)
 
     def get_author_and_date(self):
         commit = self.repo.get_commit(self.hash)
@@ -20,30 +21,56 @@ class PatchAnalyzer:
             filter(lambda x: not x.startswith(tuple(desc_tags_prefixes)), lines))
         clean_msg = "\n".join(lines_filter)
         return commit.msg, clean_msg
+    
+    def get_file_path(self):
+        commit = self.commit
+        modified_files = []
+        for modified_file in commit.modified_files:
+            file_path_new = modified_file.new_path
+            file_path_old = modified_file.old_path
+            modified_files.append(
+                {
+                    'file_path_new': file_path_new,
+                    'file_path_old': file_path_old
+                }
+            )
+        return modified_files
 
-    def get_modified_func_in_commit(self):
-        commit = self.repo.get_commit(self.hash)
-        func = []
+    def get_file_code(self):
+        pass
+
+    def get_func_name(self):
+        commit = self.commit
+        func_name = []
         try:
             for file in commit.modified_files:
                 for method in file.changed_methods:
                     method_name = method.name
-                    if method_name not in func:
-                        func.append(file.new_path+'@@'+method_name)
+                    file_path_new = file.new_path
+                    file_path_old = file.old_path
+                    func_name.append(
+                        {
+                            'file_path_new': file_path_new,
+                            'file_path_old': file_path_old,
+                            'method_name': method_name
+                        }
+                    )
+                    # if method_name not in func_name:
+                    #     func_name.append(file.new_path+'@@'+method_name)
             # self.modified_func_name = func[0]
         except Exception as e:
             ic(e, self.hash)
-        return func 
+        return func_name 
     
-    def get_modified_func_code(self):
-        commit = self.repo.get_commit(self.hash)
+    def get_func_code(self):
+        commit = self.commit
         modified_codes = {}
         try:
             for file in commit.modified_files:
                 if str(file.change_type) != 'ModificationType.MODIFY':
                     continue
                 for method in file.changed_methods:
-                    func_code_after, func_code_before = self.__get_func_of_two_versions(method, file)
+                    func_code_after, func_code_before = self.__get_func_code_of_two_versions(method, file)
                     if func_code_after and func_code_before:
                         modified_codes[method.name] = {
                             'before': func_code_before,
@@ -54,8 +81,11 @@ class PatchAnalyzer:
         except Exception as e:
             ic(e)
         return modified_codes
+
+    def get_stmt(self):
+        pass
         
-    def __get_func_of_two_versions(self, method, file):
+    def __get_func_code_of_two_versions(self, method, file):
         method_before = list(filter(lambda x: x.name==method.name, file.methods_before))
         if not method_before:
             return '', ''
@@ -80,10 +110,10 @@ class PatchAnalyzer:
         for mod_files in commit.modified_files:
             diff_code.append(mod_files.diff)
         return diff_code
-    
-    def get_modified_files(self):
-        commit = self.repo.get_commit(self.hash)
-        modified_files = []
-        for mod_files in commit.modified_files:
-            modified_files.append(mod_files.new_path)
-        return modified_files
+
+
+def test_analyzer():
+    pass
+
+if __name__=="__main__":
+    test_analyzer()
